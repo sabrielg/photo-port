7754a06e9598434ad53001dff3d8dad7484f7309
Sabriel Gee <sabrielgee@sabriels-imac.lan>
2022-03-08 17:24:46 -0700

    tests

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,46 +1,51 @@
-import React from "react";
-import { capitalizeFirstLetter } from "../../utils/helpers";
+import React from 'react';
 
 function Nav() {
-  const  categories = [
-    { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects' },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' }
+  const categories = [
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
   ];
 
-  const handleClick = () => {
-    console.log("click handled")
+  function categorySelected(name) {
+    console.log(`${name} clicked`)
   }
 
   return (
-    <header data-testid="header" className="flex-row px-1">
+    <header>
       <h2>
-        <a href="/">
+        <a data-testid="link" href="/">
           <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about" onClick={() => handleClick()}>
+            <a data-testid="about" href="#about">
               About me
             </a>
           </li>
-          <li className={"mx-2"}>
-            <span onClick={() => handleClick()}>
-              Contact
-            </span>
+          <li>
+            <span>Contact</span>
           </li>
-          {
-            categories.map((category) => (
-              <li className="mx-1" key={category.name} >
-                <span onClick={() => { handleClick(); }}>
-                 {capitalizeFirstLetter(category.name)}
+            {categories.map((category) => (
+              <li
+                className='mx-1'
+                key={category.name}
+              >
+                <span onClick={() => categorySelected(category.name)}>
+                  {category.name}
                 </span>
               </li>
-            ))
-          }
+            ))}
         </ul>
       </nav>
     </header>
